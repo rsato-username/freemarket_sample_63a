d650c7ccccd1cd749a5727abe14ed6424760ca82
rsato-username <satoheisuper@gmail.com>
2019-11-22 10:23:27 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,18 +2,26 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :omniauthable, omniauth_providers: %i[facebook google_oauth2]
   
-  validates :nickname, :tel, :description, presence: true
+  validates :nickname, :tel, presence: true
   validates :email, :tel, uniqueness: true
   validates :password, :password_confirmation, allow_nil: true, presence: true, length: { minimum: 7 }
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+    end
+  end
 
 
   has_many :items
   has_many :likes
   has_many :cards
-  belongs_to :exhibits
-  belongs_to :transactions
-  belongs_to :sale
-  belongs_to :user_info
+  # belongs_to :exhibits
+  # belongs_to :transactions
+  # belongs_to :sale
+  # belongs_to :user_info
 end
