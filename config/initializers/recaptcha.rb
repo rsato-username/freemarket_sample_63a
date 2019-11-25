483e7bfbb50021202db18d68ef3a2f0181ed1efb
rsato-username <satoheisuper@gmail.com>
2019-11-25 20:54:21 +0900

    Update recaptcha.rb

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,6 +1,6 @@
 Recaptcha.configure do |config|   
-  config.recaptcha = ENV['RECAPTCHA_SITE_KEY']
-  config.recaptcha = ENV['RECAPTCHA_SECRET_KEY']
+  config.Recaptcha = ENV['RECAPTCHA_SITE_KEY']
+  config.Recaptcha = ENV['RECAPTCHA_SECRET_KEY']
 # Uncomment the following line if you are using a proxy server:   
 # config.proxy = 'http://myproxy.com.au:8080' 
 end
