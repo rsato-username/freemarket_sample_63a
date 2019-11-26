aab8679ff9882e5219e2d2bb96b067ee73fcd977
rsato-username <satoheisuper@gmail.com>
2019-11-26 13:53:57 +0900

    up

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -9,8 +9,8 @@
 
 
 Recaptcha.configure do |config|   
-  config.site_key  = Rails.application.credentials.recaptcha[:RECAPTCHA_SITE_KEY]
-  config.secret_key = Rails.application.credentials.recaptcha[:RECAPTCHA_SECRET_KEY]
+  config.site_key  = Rails.application.credentials.recaptcha[:site_key]
+  config.secret_key = Rails.application.credentials.recaptcha[:secret_key]
 end
 
 
