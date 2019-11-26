41a3501ce4bceaa7346523955734ad9122817a64
rsato-username <satoheisuper@gmail.com>
2019-11-26 13:51:42 +0900

    Update recaptcha.rb

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -9,8 +9,8 @@
 
 
 Recaptcha.configure do |config|   
-  config.site_key  = Rails.application.credentials.recaptcha[:site_key]
-  config.secret_key = Rails.application.credentials.recaptcha[:secret_key]  
-# Uncomment the following line if you are using a proxy server:   
-# config.proxy = 'http://myproxy.com.au:8080' 
+  config.site_key  = Rails.application.credentials.recaptcha[:RECAPTCHA_SITE_KEY]
+  config.secret_key = Rails.application.credentials.recaptcha[:RECAPTCHA_SECRET_KEY]
 end
+
+
