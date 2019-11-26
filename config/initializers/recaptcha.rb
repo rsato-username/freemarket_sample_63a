42ff57806f982cf3b53a8585184f280b962212e8
rsato-username <satoheisuper@gmail.com>
2019-11-26 14:00:03 +0900

    up

--- config/initializers/recaptcha.rb
+++ config/initializers/recaptcha.rb
@@ -1,16 +1,14 @@
-# Recaptcha.configure do |config|
-#   config.site_key=Rails.application.credentials.recaptcha[:RECAPTCHA_SITE_KEY]
-#   config.secret_key=Rails.application.credentials.recaptcha[:RECAPTCHA_SECRET_KEY]
-# end
+Recaptcha.configure do |config|
+  config.site_key=Rails.application.credentials.recaptcha[:RECAPTCHA_SITE_KEY]
+  config.secret_key=Rails.application.credentials.recaptcha[:RECAPTCHA_SECRET_KEY]
+end
 
 
 # ENV['RECAPTCHA_SITE_KEY']   = Rails.application.credentials.recaptcha[:site_key]
 # ENV['RECAPTCHA_SECRET_KEY'] = Rails.application.credentials.recaptcha[:secret_key]
 
 
-Recaptcha.configure do |config|   
-  config.site_key  = Rails.application.credentials.recaptcha[:site_key]
-  config.secret_key = Rails.application.credentials.recaptcha[:secret_key]
-end
-
-
+# Recaptcha.configure do |config|   
+#   config.site_key  = Rails.application.credentials.recaptcha[:site_key]
+#   config.secret_key = Rails.application.credentials.recaptcha[:secret_key]
+# end
