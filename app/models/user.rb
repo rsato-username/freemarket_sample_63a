6352aaf52cba054be05ea8cf8e963f9508b5986b
rsato-username <satoheisuper@gmail.com>
2019-11-28 11:49:31 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,10 +25,9 @@ class User < ApplicationRecord
           )
       else
         user = User.create(
-          nickname: auth.info.nickname,
+          # nickname: auth.info.name,
           email:    auth.info.email,
           password: Devise.friendly_token[0, 20],
-          tel: ""
           )
         SnsCredential.create(
           uid: uid,
@@ -36,10 +35,65 @@ class User < ApplicationRecord
           user_id: user.id
           )
       end
+      # binding.pry
     end
     return user
   end
 
+
+
+# SNS認証情報がない場合----------------------------
+# def self.without_sns_data(auth)
+#   user = User.where(email: auth.info.email).first
+
+#   if user.present?
+#     sns = SnsCredential.create(
+#       uid: auth.uid,
+#       provider: auth.provider,
+#       user_id: user.id
+#     )
+#   else
+#     user = User.new(
+#       nickname: auth.info.name,
+#       email: auth.info.email,
+#     )
+#     sns = SnsCredential.new(
+#       uid: auth.uid,
+#       provider: auth.provider
+#     )
+#   end
+
+#   return { user: user ,sns: sns}
+# end
+
+# # SNS認証情報がある場合
+# def self.with_sns_data(auth, snscredential)
+#   user = User.where(id: snscredential.user_id).first
+#   unless user.present?
+#     user = User.new(
+#       nickname: auth.info.name,
+#       email: auth.info.email,
+#     )
+#   end
+#   return {user: user}
+# end
+
+# def self.find_oauth(auth)
+#   uid = auth.uid
+#   provider = auth.provider
+#   snscredential = SnsCredential.where(uid: uid, provider: provider).first
+#   # SNS認証情報があるかないか？
+#   if snscredential.present?
+#     user = with_sns_data(auth, snscredential)[:user]
+#     sns = snscredential
+#   else
+#     user = without_sns_data(auth)[:user]
+#     sns = without_sns_data(auth)[:sns]
+#   end
+#   return { user: user ,sns: sns}
+# end
+
+#------------------------------------------------------
 
   has_many :items
   has_many :likes
