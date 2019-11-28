ba8462715e49cde7e64b026602b16b710d1edd00
you722t <you.t-22exl@ezweb.ne.jp>
2019-11-28 11:43:31 +0900

    up

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,13 +1,19 @@
 class ItemsController < ApplicationController
 
   def index
-    @parents = Category.all
   end
 
   def new
+    @item = Item.new
   end
   
   def create
+    Item.create(item_params)
+    redirect_to root_path
   end
   
+  private
+  def item_params
+    params.require(:item).permit(:name, :price, :description, :status, :post_money, :post_region, :post_day, :user_id, :category_id, :brand_id)
+  end
 end
