acf2b6229fcf276875118390eeecaf459d68068a
you722t <you.t-22exl@ezweb.ne.jp>
2019-11-28 20:42:55 +0900

    カテゴリ表示

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,14 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root to: 'items#index'
 
-  resources :items, only: [:index, :new, :create]
+  resources :items, only: [:index, :new, :create] do
+    collection do
+      get 'get_category_children', defaults: { format: 'json' }
+      get 'get_category_grandchildren', defaults: { format: 'json' }
+    end
+  end
+
   resources :users, only: [:index]
+
+  
 end
