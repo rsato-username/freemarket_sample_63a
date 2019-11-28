8c57178458e2311732ddfd8b0dbfb967c25602c1
rsato-username <satoheisuper@gmail.com>
2019-11-28 11:49:28 +0900

    Update omniauth_callbacks_controller.rb

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -8,16 +8,39 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     callback_for(:google)
   end
 
+
+  private
+
   def callback_for(provider)
     @user = User.find_oauth(request.env["omniauth.auth"])
     if @user.persisted?
-      sign_in_and_redirect @user, event: :authentication #after_sign_in_path_forと同じパス
+      sign_in_and_redirect @user, event: :authentication
       set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
     else
       session["devise.#{provider}_data"] = request.env["omniauth.auth"].except("extra")
       redirect_to new_user_registration_path
     end
   end
+
+
+#========================================================#
+
+  # def callback_for(provider)
+  #   @omniauth = request.env['omniauth.auth']
+  #   info = User.find_oauth(@omniauth)
+  #   @user = info[:user]
+  #   if @user.persisted?
+  #     sign_in_and_redirect @user, event: :authentication
+  #     set_flash_message(:notice, :success, kind: "#{provider}".capitalize) if is_navigational_format?
+  #   else
+  #     @sns = info[:sns]
+  #     session[:provider] = info[:sns][:provider]
+  #     session[:uid] = info[:sns][:uid]
+  #     render step1_signups_path
+  #   end
+  # end
+
+  #=============================================================#
 
   def failure
     redirect_to root_path and return
