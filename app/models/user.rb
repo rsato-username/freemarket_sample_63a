78206ef7422058ae594c52c6b866afb6b70dbc9e
rsato-username <satoheisuper@gmail.com>
2019-11-28 11:51:23 +0900

    Update user.rb

--- app/models/user.rb
+++ app/models/user.rb
@@ -25,7 +25,7 @@ class User < ApplicationRecord
           )
       else
         user = User.create(
-          # nickname: auth.info.name,
+          nickname: auth.info.name,
           email:    auth.info.email,
           password: Devise.friendly_token[0, 20],
           )
