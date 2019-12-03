e01b90cc6a6a728256d870f982502940d3b1be8b
rsato-username <satoheisuper@gmail.com>
2019-12-03 10:21:38 +0900

    Update schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_12_02_023601) do
+ActiveRecord::Schema.define(version: 2019_12_02_104259) do
 
   create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.bigint "user_id", null: false
@@ -66,7 +66,7 @@ ActiveRecord::Schema.define(version: 2019_12_02_023601) do
     t.string "kan_firstname", null: false
     t.string "kana_familyname", null: false
     t.string "kana_firstname", null: false
-    t.integer "birthday"
+    t.date "birthday"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
