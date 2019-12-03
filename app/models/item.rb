60ca3f278410cb0ea06df9d0643d2582681b83e0
you722t <you.t-22exl@ezweb.ne.jp>
2019-12-03 12:13:51 +0900

    photoモデルに同時に写真が保存できるようになった

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,6 @@
 class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  has_many   :photos
+  accepts_nested_attributes_for :photos, allow_destroy: true
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_11_28_043122) do
+ActiveRecord::Schema.define(version: 2019_12_03_013441) do
 
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
@@ -35,6 +35,14 @@ ActiveRecord::Schema.define(version: 2019_11_28_043122) do
     t.datetime "updated_at", null: false
     t.index ["category_id"], name: "index_items_on_category_id"
     t.index ["user_id"], name: "index_items_on_user_id"
+  end
+
+  create_table "photos", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.string "url", null: false
+    t.bigint "item_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["item_id"], name: "index_photos_on_item_id"
   end
 
   create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -67,5 +75,6 @@ ActiveRecord::Schema.define(version: 2019_11_28_043122) do
 
   add_foreign_key "items", "categories"
   add_foreign_key "items", "users"
+  add_foreign_key "photos", "items"
   add_foreign_key "sns_credentials", "users"
 end
