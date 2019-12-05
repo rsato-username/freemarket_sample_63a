c304f93639765137274373c190492e87b21147b5
you722t <you.t-22exl@ezweb.ne.jp>
2019-12-05 21:01:27 +0900

    投稿はできる

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -5,10 +5,10 @@ class ItemsController < ApplicationController
 
   def new
     @item = Item.new
-    @item.photos.build
-    
+    10.times{
+      @item.photos.build
+    }
     @category_parent_array = []
-    #データベースから、親カテゴリーのみ抽出し、配列化
     Category.where(ancestry: nil).each do |parent|
         @category_parent_array << parent
     end
@@ -16,7 +16,6 @@ class ItemsController < ApplicationController
   
   def create
     @item = Item.new(item_params)
-    binding.pry()
     if @item.save
       redirect_to root_path
     else
@@ -24,13 +23,11 @@ class ItemsController < ApplicationController
     end
   end
 
-  # 親カテゴリーが選択された後に動くアクション
   def get_category_children
     #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
     @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
   end
 
- # 子カテゴリーが選択された後に動くアクション
   def get_category_grandchildren
     #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
     @category_grandchildren = Category.find("#{params[:child_id]}").children
@@ -38,6 +35,6 @@ class ItemsController < ApplicationController
   
   private
   def item_params
-    params.require(:item).permit(:name, :price, :description, :status, :post_money, :post_region, :post_day, :brand, :category_id, :user_id, photos_attributes: [:url]).merge(user_id: current_user.id)
+    params.require(:item).permit(:name, :price, :description, :status, :post_money, :post_region, :post_day, :brand, :category_id, :user_id, photos_attributes:[:url]).merge(user_id: current_user.id)
   end
 end
