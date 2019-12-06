cca8b81d31b689af944d3442e88fdc0028fe61c4
aki-23 <56856857+aki-23@users.noreply.github.com>
2019-12-06 20:42:33 +0900

    Update items_controller.rb

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,24 +34,15 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
-  def get_category_children
-    #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
-    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
-  end
-
-  def get_category_grandchildren
-    #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
-    @category_grandchildren = Category.find("#{params[:child_id]}").children
-  end
-
-  def show
+  def buy
+    @item = Item.find(params[:id])
   end
 
   def pay
     Payjp.api_key = ENV['PAYJP_ACCESS_KEY']
-    card = current_user.cards.first
+    card = current_user.card.first
     if card.blank?
-      redirect_to pay_cards_path
+      redirect_to confirmation_card_path
     else
       @item = Item.find(params[:id])
       Payjp::Charge.create(
@@ -61,6 +52,16 @@ class ItemsController < ApplicationController
       )
     end
     redirect_to items_path
+  end
+
+  def get_category_children
+    #選択された親カテゴリーに紐付く子カテゴリーの配列を取得
+    @category_children = Category.find_by(name: "#{params[:parent_name]}", ancestry: nil).children
+  end
+
+  def get_category_grandchildren
+    #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
+    @category_grandchildren = Category.find("#{params[:child_id]}").children
   end
   
   private
