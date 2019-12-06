23215962eebf13f84c4027c5395da4bed321aa5f
rsato-username <satoheisuper@gmail.com>
2019-12-06 15:57:35 +0900

    up item-show

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,15 @@
 
 ActiveRecord::Schema.define(version: 2019_12_03_013441) do
 
+  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.string "customer_id", null: false
+    t.string "card_id", null: false
+    t.index ["user_id"], name: "index_cards_on_user_id"
+  end
+
   create_table "categories", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "name"
     t.datetime "created_at", null: false
@@ -43,14 +52,6 @@ ActiveRecord::Schema.define(version: 2019_12_03_013441) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["item_id"], name: "index_photos_on_item_id"
-
-  create_table "cards", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
-    t.bigint "user_id", null: false
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "customer_id", null: false
-    t.string "card_id", null: false
-    t.index ["user_id"], name: "index_cards_on_user_id"
   end
 
   create_table "sns_credentials", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
@@ -103,11 +104,10 @@ ActiveRecord::Schema.define(version: 2019_12_03_013441) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "cards", "users"
   add_foreign_key "items", "categories"
   add_foreign_key "items", "users"
   add_foreign_key "photos", "items"
-
-  add_foreign_key "cards", "users"
   add_foreign_key "sns_credentials", "users"
   add_foreign_key "user_infos", "users"
 end
