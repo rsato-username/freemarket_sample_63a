a7cebe20632accb499eb869bf98a32ee476255c3
rsato-username <satoheisuper@gmail.com>
2019-12-07 16:16:18 +0900

    up

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -44,6 +44,8 @@ class ItemsController < ApplicationController
   def get_category_grandchildren
     #選択された子カテゴリーに紐付く孫カテゴリーの配列を取得
     @category_grandchildren = Category.find("#{params[:child_id]}").children
+  end
+  
   def buy
     @item = Item.find(params[:id])
     Payjp.api_key = ENV['PAYJP_ACCESS_KEY']
