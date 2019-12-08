f96a118d58705c89e3b501e2c8607f60201a3a07
aki-23 <56856857+aki-23@users.noreply.github.com>
2019-12-08 16:33:01 +0900

    model up

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,9 @@
 class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
-  has_many   :photos
+  has_many   :photos,  foreign_key: :item_id, dependent: :destroy
   accepts_nested_attributes_for :photos, allow_destroy: true
+  
 
   def self.search(search)
     if search
@@ -15,7 +16,7 @@ class Item < ApplicationRecord
   def previous
     Item.where("id < ?", self.id).order("id DESC").first
   end
- 
+
   def next
     Item.where("id > ?", self.id).order("id ASC").first
   end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,4 @@
 class Photo < ApplicationRecord
-  belongs_to :item
-
+  belongs_to :item, optional: true
   mount_uploader :url, ImageUploader
 end
