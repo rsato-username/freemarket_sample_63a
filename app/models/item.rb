36c522eae02026e175441cf90d2b6ef9116d00de
aki-23 <56856857+aki-23@users.noreply.github.com>
2019-12-09 16:19:06 +0900

    model up

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,8 +1,11 @@
 class Item < ApplicationRecord
   belongs_to :user
   belongs_to :category
+  belongs_to :brand, optional: true
   has_many   :photos,  foreign_key: :item_id, dependent: :destroy
+  accepts_nested_attributes_for :brand, reject_if: :reject_both_blank
   accepts_nested_attributes_for :photos, allow_destroy: true
+
   
 
   def self.search(search)
@@ -20,4 +23,14 @@ class Item < ApplicationRecord
   def next
     Item.where("id > ?", self.id).order("id ASC").first
   end
+
+  def reject_both_blank(attributes)
+    if attributes[:id]
+      attributes.merge!(_destroy: "1") if attributes[:name].blank?
+      !attributes[:name].blank?
+    else
+      attributes[:name].blank?
+    end
+  end
+  
 end
