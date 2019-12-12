fafa1624f7da415fd912418c7c85c840f668eea8
you722t <you.t-22exl@ezweb.ne.jp>
2019-12-12 19:03:41 +0900

    up

--- app/assets/javascripts/file_preview.js
+++ app/assets/javascripts/file_preview.js
@@ -1,18 +1,14 @@
 
 $(function(){
   $('#item_images').on('change', function(e) {
+    var preview = $(".exhibit__container--form__image--preview");
+    preview.empty();
     for (var i = 0, file; file = e.target.files[i]; i++) {
-      // file = e.target.files[0]
-      reader = new FileReader(),
-      $preview = $(".exhibit__container--form__image--preview");
-      reader.onload = (function(file) {
+      var reader = new FileReader();
+      reader.onload = (function() {
         return function(e) {
-          $preview.empty();
-          $preview.append($('<img>').attr({
+          preview.append($('<img>').attr({
             src: e.target.result,
-            width: "20%",
-            // class: "preview",
-            title: file.name
           }));
         };
       })(file);
@@ -20,4 +16,27 @@ $(function(){
     }
   });
 });
+
+// $(function(){
+//   var array = [];
+//   $('#item_images').on('change', function(e) {
+//     var preview = $(".exhibit__container--form__image--preview");
+//     preview.empty();
+//     for (var i = 0, file; file = e.target.files[i]; i++) {
+//       array.push(file);
+//     }
+//     for (var i = 0, f; f = array[i]; i++) {
+//       var reader = new FileReader();
+//       reader.onload = (function() {
+//         return function(e) {
+//           preview.append($('<img>').attr({
+//             src: e.target.result,
+//           }));
+//         };
+//       })();
+//       reader.readAsDataURL(f);
+//       // new FileUpload();
+//     }
+//   });
+// });
 
